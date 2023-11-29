0c3918139b84e35f3b08b69a0d74be0786eb92ad
Nicholas Papierniak <nick.papierniak@gmail.com>
2023-11-29 09:20:46 -0600

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Nicholas Papierniak on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
